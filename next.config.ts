e58d3de6995e66098d60a6f9915c96af07354eea
salahu <salahu@gmail.com>
2026-01-29 23:48:05 +0530

    feat: Configure Next.js project for deployment to GitHub Pages by adding deploy scripts, `gh-pages` dependency, and production-specific base paths.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/gif-explorer' : undefined,
+  assetPrefix: isProd ? '/gif-explorer/' : undefined,
 };
 
 export default nextConfig;
