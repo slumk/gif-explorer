4955e9f356b8d9d8bc50a173b85f1c8b09c4c3dd
salahu <salahu@gmail.com>
2026-01-29 23:47:55 +0530

    Updates

---
+++ _next/static/mQV2p6M2rU7s1sJSAo2CI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gif-explorer//_next/:path+",
+        "destination": "/gif-explorer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
